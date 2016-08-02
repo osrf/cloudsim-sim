9992c325c5d14faa59ee15e62f53f04e79d863bc
Hugo Boyer <hugo@osrfoundation.org>
2016-08-02 13:14:24 -0700

    authenticate and resource

--- server/simulations.js
+++ server/simulations.js
@@ -4,17 +4,16 @@ const express = require('express')
 const router = express.Router()
 const csgrant = require('cloudsim-grant')
 
-
 function setRoutes(app) {
-
   console.log('SIMULATIONS setRoutes')
+  app.get('/simulations',
+    csgrant.authenticate,
+    csgrant.ownsResource('simulation_list', false),
+    function(req, res) {
 
-  // router.route('/simulations')
-  app.get('/simulations',function(req, res) {
     console.log('GET all simulations')
-    const userToken = req.query.token
-    console.log('  req.query.token: ' + userToken)
-    csgrant.readAllResourcesForUser(userToken, (err, items) => {
+
+    csgrant.readAllResourcesForUser(req.user, (err, items) => {
       const r = {success: false, operation: 'getSimulations'}
       if(err) {
         r.error = err
@@ -28,187 +27,102 @@ function setRoutes(app) {
   })
 
   // create a new simulation
-  app.post('/simulations', csgrant.authenticate, function(req, res) {
+  app.post('/simulations',
+           csgrant.authenticate,
+           csgrant.ownsResource('simulation_list', false),
+           function(req, res) {
 
-    const token = req.headers.authorization
-    if (!token) {
-      console.log('no token in req.headers.authorization')
-    }
-    else {
-      console.log('tokenito: ', token)
-    }
     console.log('create sim:')
     console.log('  body:' +  JSON.stringify(req.body))
     console.log('  query:' + JSON.stringify(req.query))
 
-    // step 1, user verify
-
-    // The data for the new
-    const resourceData = {cmd: req.query.cmd}
-
+    const data = req.query
+    const resourceData = {cmd: data.cmd, auto: data.auto}
     const error = function(msg) {
       return {operation: 'createSimulation',
               success: false,
               error: msg}
     }
     const op = 'createSimulation'
-    csgrant.verifyToken(token, (err, decoded) => {
+    const user = req.user
+    const r = {success: false}
+    csgrant.getNextResourceId('sim', (err, resourceName) => {
       if(err) {
-        return res.jsonp( error('invalid token'))
+        res.jsonp(error(err))
+        return
       }
-      const user = decoded.username
-      // step 2.  is user allowed?
-      csgrant.isAuthorized(user, 'simulation_list', false,
-                          (err, authorized) => {
+      // step 4. add simulation
+      csgrant.createResource(user, resourceName, resourceData,
+                            (err, data) => {
         if(err) {
-          return res.jsonp(error(err))
+          res.jsonp(error(err))
+          return
         }
-        if(!authorized){
-          const msg = 'insufficient permission for user "' + user + '"'
-          return res.jsonp(error(msg))
-        }
-        // step 3. get unique id
-        const r = {success: false}
-        csgrant.getNextResourceId('sim', (err, resourceName) => {
-          if(err) {
-            res.jsonp(error(err))
-            return
-          }
-          // step 4. add simulation
-          csgrant.createResource(user, resourceName, resourceData,
-                                (err, data) => {
-            if(err) {
-              res.jsonp(error(err))
-              return
-            }
-            // step 5. success!
-            const r = { success: true,
-                        operation: op,
-                        result: data,
-                        id: resourceName}
-            res.jsonp(r)
-          })
-        })
-      })
-    })
-  })
-
-  // read a simulation
-  app.get('/simulations/:simId', function(req, res) {
-    const resourceName = req.simId
-    console.log('read /simulations/:simId = ' + resourceName)
-    const token = req.body.token
-    csgrant.verifyToken(token, (err, decoded) => {
-      if(err) {
-        return res.jsonp( {success: false, error: err})
-      }
-      const user = decoded.username
-      // step 2.  is user allowed (read only)?
-      csgrant.isAuthorized(user, resourceName, true, (err, authorized) => {
-        if(err) {
-          return res.jsonp({success: false, error: err})
-        }
-        if(!authorized){
-          const msg = 'insufficient permission for user "' + user + '"'
-          return res.jsonp({success: false, error: msg})
-        }
-        // step 3. get simulation
-        const r = {success: false}
-        csgrant.getResource(user, resource, (err, data) => {
-          if(err) {
-            return res.jsonp({success: false, error: err})
-          }
-          r.success = true
-          r.result = data
-          // success
-          res.jsonp(r)
-        })
+        // step 5. success!
+        const r = { success: true,
+                    operation: op,
+                    result: data,
+                    id: resourceName}
+        res.jsonp(r)
       })
     })
   })
 
   // Update a simulation
-  app.put('/simulations/:simId', function(req, res) {
-    const resourceName = req.simId
-    const newData = req.data
-    console.log('Update simulation: ' + resourceName)
-    console.log(' new data: ' + newData)
+  app.put('/simulations/:simId',
+          csgrant.authenticate,
+          csgrant.ownsResource(':simId', true),
+          function(req, res) {
 
-    const token = req.body.token
-    csgrant.verifyToken(token, (err, decoded) => {
+    const resourceName = req.simId
+    const newData = req.query
+    console.log(' Update simulation: ' + resourceName)
+    console.log(' new data: ' + JSON.stringify(newData))
+    const user = req.user
+
+    const r = {success: false}
+    if (!newData.cmd) {
+       return res.jsonp({success: false, error: 'invalid new simulation: missing cmd'})
+    }
+    if (!newData.auto) {
+       return res.jsonp({success: false, error: 'invalid new simulation: missing auto'})
+    }
+
+    csgrant.updateResource(user, resourceName, newData, (err, data) => {
       if(err) {
-        return res.jsonp( {success: false, error: err})
+        return res.jsonp({success: false, error: err})
       }
-      const user = decoded.username
-      // step 2.  is user allowed (write perm) ?
-      csgrant.isAuthorized(user, resourceName, false, (err, authorized) => {
-        if(err) {
-          return res.jsonp({success: false, error: err})
-        }
-        if(!authorized){
-          const msg = 'insufficient permission for user "' + user + '"'
-          return res.jsonp({success: false, error: msg})
-        }
-        // step 3. get simulation
-        const r = {success: false}
-        csgrant.updateResource(user, resource, newData, (err, data) => {
-          if(err) {
-            return res.jsonp({success: false, error: err})
-          }
-          r.success = true
-          r.result = data
-          // success
-          res.jsonp(r)
-        })
-      })
+      r.success = true
+      r.result = data
+      // success
+      res.jsonp(r)
     })
   })
 
   // Delete a simulation
-  app.delete('/simulations/:simId', function(req, res) {
+  app.delete('/simulations/:simId',
+             csgrant.authenticate,
+             csgrant.ownsResource(':simId', false),
+             function(req, res) {
     console.log('delete simulation ' + req.simId)
     const resourceName = req.simId
-    const newData = req.data
-    console.log('Update simulation: ' + resourceName)
-    console.log(' new data: ' + newData)
-
-    const token = req.body.token
-    csgrant.verifyToken(token, (err, decoded) => {
+    const r = {success: false}
+    const user = req.user  // from previous middleware
+    const resource = req.simId // from app.param (see below)
+    csgrant.deleteResource(user, resource, (err, data) => {
       if(err) {
-        return res.jsonp( {success: false, error: err})
+        return res.jsonp({success: false, error: err})
       }
-      const user = decoded.username
-      // step 2.  is user allowed (write perm) ?
-      csgrant.isAuthorized(user, resourceName, false, (err, authorized) => {
-        if(err) {
-          return res.jsonp({success: false, error: err})
-        }
-        if(!authorized){
-          const msg = 'insufficient permission for user "' + user + '"'
-          return res.jsonp({success: false, error: msg})
-        }
-        // step 3. get simulation
-        const r = {success: false}
-        csgrant.deleteResource(user, resource, (err, data) => {
-          if(err) {
-            return res.jsonp({success: false, error: err})
-          }
-          r.success = true
-          r.result = data
-          // success
-          res.jsonp(r)
-        })
-      })
+      r.success = true
+      r.result = data
+      // success
+      res.jsonp(r)
     })
   })
 
   // simId
   app.param('simId', function( req, res, next, id) {
-    console.log('   Resolving sim id: ' + id)
-
-    if (true) {
-      req.simId = id
-    }
+    req.simId = id
     next()
   })
 }
