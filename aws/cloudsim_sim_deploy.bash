a8190fffe127d96d599562b1209cc75d0d92292b
hugomatic <devnull@localhost>
2017-01-10 04:39:58 +0000

    look for ssh key route in options.json

--- aws/cloudsim_sim_deploy.bash
+++ aws/cloudsim_sim_deploy.bash
@@ -6,10 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 simdir="$DIR/.."
 codedir="$DIR/../.."
 # Helper to parse options from cloudsim-options.json
-#get_option(){
-#  echo `node -pe "var f = \"$1\"; var query = \"$2\"; var j=require(f); j[query] "`
-#}
+get_option(){
+  echo `node -pe "var f = \"$1\"; var query = \"$2\"; var j=require(f); j[query] "`
+}
 
+codedir="$DIR/../.."
+optionsfile=$codedir/cloudsim-options.json
+ssh_route=`get_option $optionsfile ssh`
+
+# if ssh_route is valid, download the key and place it in the server directory
+if [ "$ssh_route" != "undefined" ]; then  
+  echo "ssh key [$ssh_route]"
+  curl -X GET --header 'Accept: application/json' --header "authorization: $token" $ssh_route > $codedir/sshkeys.zip
+  cp $codedir/sshkeys.zip $simdir/server/keys.zip
+fi
 
 cp $codedir/cloudsim-env.bash $simdir/.env
 cp $codedir/cloudsim-options.json $simdir/options.json
