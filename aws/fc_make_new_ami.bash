02c1534108b5de83b822456f1eb7bbad6787d24c
Patricio Reyna Almandos <preyna@ekumenlabs.com>
2017-04-27 16:04:59 -0300

    minor udpate

--- aws/fc_make_new_ami.bash
+++ aws/fc_make_new_ami.bash
@@ -13,6 +13,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/make_new_ami.bash
 
+# Remove ssh deploy key
+rm -f ~/.ssh/deploy_key_rsa
+
 # stop and remove docker containers
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
