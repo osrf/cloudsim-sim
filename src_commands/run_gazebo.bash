03d337e72c07120e120ee17f35fead6afb251019
Steve Peters <scpeters@osrfoundation.org>
2017-04-28 16:42:03 -0700

    Backed out changeset f23661defc45

--- src_commands/run_gazebo.bash
+++ src_commands/run_gazebo.bash
@@ -17,7 +17,6 @@ cat <<DELIM > launch_server.bash
 # (example with log) roslaunch srcsim finals.launch final_number:=2 extra_gazebo_args:="-r --record_path ~/gazebo-logs/myworldlog"
 source /opt/nasa/indigo/setup.bash
 #exec roslaunch srcsim unique.launch init:="true"
-export ROS_HOSTNAME=192.168.2.1
 GAZEBO_IP_WHITE_LIST=127.0.0.1 exec roslaunch srcsim unique.launch init:="true" extra_gazebo_args:="-r --record_path /home/cloudsim/gazebo-logs/$WORLD_NAME"
 DELIM
 chmod a+x launch_server.bash
