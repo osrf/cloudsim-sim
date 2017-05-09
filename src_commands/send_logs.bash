b74023587be65c5a5dc083b4d34914910a185ee6
Patricio Reyna Almandos <preyna@ekumenlabs.com>
2017-05-09 11:44:30 -0300

    reanmed task by world

--- src_commands/send_logs.bash
+++ src_commands/send_logs.bash
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-TASK_NAME=$1
+WORLD_NAME=$1
 s3dir="/home/ubuntu/s3"
-logsdir="/home/ubuntu/code/gazebo-logs/$TASK_NAME"
+logsdir="/home/ubuntu/code/gazebo-logs/$WORLD_NAME"
 s3mountdir="/mnt/s3bucket"
 if [ -f $s3dir/bucketname-s3fs.txt ]
 then
     echo 'uploading logs'
-    mkdir -p $s3mountdir/$TASK_NAME
-    cp -r $logsdir $s3mountdir/$TASK_NAME
+    mkdir -p $s3mountdir/$WORLD_NAME
+    cp -r $logsdir $s3mountdir/$WORLD_NAME
     echo 'finished copying logs to s3 mnt folder'
 else
     echo 'S3 is not configured. Cancel log uploading'
