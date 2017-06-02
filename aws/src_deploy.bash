371dc71fdc5e9e5cc51c9d940008f829a93548fa
Patricio Reyna Almandos <preyna@ekumenlabs.com>
2017-06-02 09:36:21 -0300

    more tests with ips

--- aws/src_deploy.bash
+++ aws/src_deploy.bash
@@ -111,7 +111,7 @@ elif [ $role == "fieldcomputer" ]; then
 
   echo "about to setup bridge for vpn"
   # Create docker network (vpn-br0) and corresponding OS bridge (br0)
-  docker network create --driver=bridge --ip-range=192.168.2.9/31 --subnet=192.168.2.0/24 -o "com.docker.network.bridge.name=br0" vpn-br0
+  docker network create --driver=bridge --ip-range=192.168.2.9/32 --subnet=192.168.2.0/24 -o "com.docker.network.bridge.name=br0" vpn-br0
 
   # Make the client come back up on reboot
   cat << EOF > /etc/rc.local
@@ -154,7 +154,7 @@ EOF
   brctl addif br0 tap0
   brctl setfd br0 0
   ifconfig tap0 0.0.0.0 up
-  ifconfig br0 192.168.2.9 netmask 255.255.255.0 broadcast 192.168.2.255
+  #ifconfig br0 192.168.2.9 netmask 255.255.255.0 broadcast 192.168.2.255
 
   #  Notify cloudsim-sim server that the team's image has been built
   curl -X POST --header "Content-Type: application/json" --header 'Accept: application/json' --header "authorization: $token" --data '{"fc_docker_image":"fcomputer"}' "http://localhost:4000/events"
