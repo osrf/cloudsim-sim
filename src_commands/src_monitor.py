4f94ec3d4eb5339cf9742c90ce424faeccc47ade
Ian Chen <ichen@osrfoundation.org>
2017-06-06 13:38:28 -0700

    monitor harness

--- src_commands/src_monitor.py
+++ src_commands/src_monitor.py
@@ -13,6 +13,8 @@ import time
 import rospy
 from srcsim.msg import Task
 from srcsim.msg import Score
+from srcsim.msg import Harness
+
 from sensor_msgs.msg import JointState
 from nav_msgs.msg import Odometry
 
@@ -26,14 +28,10 @@ score = 0
 prevScore = 0
 totalCompletionTime = 0
 
-scriptDir = os.path.dirname(os.path.realpath(__file__))
+# harness monitor variable
+prevHarnessStatus = -1
 
-# A condition variable for each sim topic to monitor
-cvs = [threading.Condition(), threading.Condition()]
-# Message count for each topic
-topicsMsgCount = [0, 0]
-topicsWaitSleepTime = 5
-sleepTimeForMessageCheck = 30
+scriptDir = os.path.dirname(os.path.realpath(__file__))
 
 
 def postToSim():
@@ -53,49 +51,50 @@ def postToSim():
 
 def postData(dataJson):
 
-  rospy.loginfo("dataJson: %s", dataJson)
+  rospy.logwarn("dataJson: %s", dataJson)
   try:
     headers = {'Content-type': 'application/json', 'Authorization': token}
     url="http://localhost:4000/events"
     response = requests.post(url, data=dataJson, headers=headers)
     if response.status_code != 200:
-      rospy.loginfo('Unexpected response: %i ', response.status_code)
-    rospy.loginfo('Response: %s ', response.text)
+      rospy.logwarn('Unexpected response: %i ', response.status_code)
+    rospy.logwarn('Response: %s ', response.text)
   except:
-    rospy.loginfo('Unable to post to cloudsim-sim. Is server running?')
+    rospy.logwarn('Unable to post to cloudsim-sim. Is server running?')
 
 
-def postSimStatus(data):
+def postControllerStatus(data):
   dataJson = json.dumps({"simulator_ready": data})
   postData(dataJson)
 
-def checkSimStatus():
+def checkControllerStatus():
 
-  global topicsMsgCount, sleepTimeForMessageCheck, topicsWaitSleepTime, cvs
+  topicsWaitSleepTime = 20
+  sleepTimeForMessageCheck = 15
 
   # Wait for a bit
   time.sleep(topicsWaitSleepTime)
-  rospy.loginfo('Started monitoring controller topics')
+  rospy.logwarn('Started monitoring controller topics')
 
   timeout = 120
   try:
     rospy.wait_for_message("/ihmc_ros/valkyrie/output/robot_pose", Odometry, timeout)
   except(rospy.ROSException), e:
-    rospy.loginfo("ihmc robot_pose topic not available, aborting...")
-    postSimStatus(-1)
+    rospy.logwarn("Failed to receive ihmc robot_pose message, aborting...")
+    postControllerStatus(-1)
     return
 
-  rospy.loginfo('Received ihmc robot_pose message')
+  rospy.logwarn('Received ihmc robot_pose message')
 
   try:
-    rospy.wait_for_message("/joint_states", JointState, 10)
+    rospy.wait_for_message("/joint_states", JointState, timeout)
   except(rospy.ROSException), e:
-    rospy.loginfo("Joint states topic not available, aborting...")
+    rospy.logwarn("Failed to receive koint states message, aborting...")
     postSimStatus(-1)
     return
 
-  rospy.loginfo('Received joint states message')
-  rospy.loginfo('Sleeping for more checks')
+  rospy.logwarn('Received joint states message')
+  rospy.logwarn('Sleeping for more checks')
 
   time.sleep(sleepTimeForMessageCheck)
 
@@ -104,12 +103,42 @@ def checkSimStatus():
     rospy.wait_for_message("/joint_states", JointState, 10)
     rospy.wait_for_message("/ihmc_ros/valkyrie/output/robot_pose", Odometry, 10)
   except(rospy.ROSException), e:
-    rospy.loginfo("Failed to receive more messages, aborting...")
-    postSimStatus(-1)
+    rospy.logwarn("Failed to receive more messages, aborting...")
+    postControllerStatus(-1)
     return
 
-  rospy.loginfo("Valid simulation")
-  postSimStatus(1)
+  rospy.logwarn("Valid simulation")
+  postControllerStatus(1)
+
+def postHarnessStatus(data):
+  dataJson = json.dumps({"harness_status": data})
+  postData(dataJson)
+
+def simHarnessCallback(data):
+
+  global prevHarnessStatus
+
+  harnessStatus = data.status
+  if harnessStatus == prevHarnessStatus:
+    return
+
+  postHarnessStatus(data.status)
+  prevHarnessStatus = harnessStatus
+
+def checkHarnessStatus():
+
+  topicWaitSleepTime = 100
+
+  # Wait for a bit
+  time.sleep(topicWaitSleepTime)
+  rospy.logwarn('Started monitoring harness topic')
+
+  rospy.Subscriber("/srcsim/finals/harness", Harness, simHarnessCallback)
+
+  # monitor for whole sim duration
+  while True:
+    time.sleep(10)
+
 
 def simTaskCallback(data):
 
@@ -137,16 +166,16 @@ def simTaskCallback(data):
     return
 
   # log output
-  rospy.loginfo("task: %u", taskId)
-  rospy.loginfo("current_checkpoint: %u", currentCheckPoint)
-  rospy.loginfo("start_time: %f", startTime)
-  rospy.loginfo("elapsed_time: %f", elapsedTime)
-  rospy.loginfo("timed_out: %s", timedOut)
-  rospy.loginfo("finished: %s", finished)
+  rospy.logwarn("task: %u", taskId)
+  rospy.logwarn("current_checkpoint: %u", currentCheckPoint)
+  rospy.logwarn("start_time: %f", startTime)
+  rospy.logwarn("elapsed_time: %f", elapsedTime)
+  rospy.logwarn("timed_out: %s", timedOut)
+  rospy.logwarn("finished: %s", finished)
   for i in range(len(checkpoint_durations)):
-    rospy.loginfo("checkpoint_durations %i: %f", i, checkpoint_durations[i])
+    rospy.logwarn("checkpoint_durations %i: %f", i, checkpoint_durations[i])
   for i in range(len(checkpoint_penalties)):
-    rospy.loginfo("checkpoint_penalties %i: %f", i, checkpoint_penalties[i])
+    rospy.logwarn("checkpoint_penalties %i: %f", i, checkpoint_penalties[i])
 
   mutex.acquire()
   # update tasks
@@ -164,7 +193,7 @@ def simTaskCallback(data):
   mutex.release()
 
   # post to cloudsim-sim
-  rospy.loginfo("== Posting TASK data ==")
+  rospy.logwarn("== Posting TASK data ==")
   postToSim()
 
 
@@ -185,7 +214,7 @@ def simScoreCallback(data):
   mutex.release()
 
   # post to cloudsim-sim
-  rospy.loginfo("== Posting SCORE data ==")
+  rospy.logwarn("== Posting SCORE data ==")
   postToSim()
 
 
@@ -215,8 +244,8 @@ def fcTaskCallback(data):
 
   cmd = scriptDir + "/src_tc.rb"
 
-  rospy.loginfo("task: %u", taskId)
-  rospy.loginfo("uplink/donwlink: %s/%s", uplink, downlink)
+  rospy.logwarn("task: %u", taskId)
+  rospy.logwarn("uplink/donwlink: %s/%s", uplink, downlink)
 
   out = subprocess.Popen(["sudo", cmd, "-i", "tap0", "-u", uplink, "-d", downlink, "-f", "192.168.2.150/26"])
 
@@ -228,7 +257,7 @@ def main():
     sys.exit()
 
   role = sys.argv[1]
-  rospy.loginfo("role: %s", role)
+  rospy.logwarn("role: %s", role)
 
   rospy.init_node('task_monitor', anonymous=True)
 
@@ -238,8 +267,8 @@ def main():
       sys.exit()
     token = sys.argv[2]
     roundNumber = sys.argv[3]
-    rospy.loginfo("token: %s", token)
-    rospy.loginfo("round number: %s", roundNumber)
+    rospy.logwarn("token: %s", token)
+    rospy.logwarn("round number: %s", roundNumber)
     roundName = "round_" + str(roundNumber)
 
     mutex = threading.Lock()
@@ -249,9 +278,14 @@ def main():
     rospy.Subscriber("/srcsim/finals/score", Score, simScoreCallback)
 
     # Subscribe to topics for checking sim launch process
-    postSimStatus(0)
-    t = thread.Thread(target=checkSimStatus)
-    t.start()
+    postControllerStatus(0)
+    controllerThread = threading.Thread(target=checkControllerStatus)
+    controllerThread.start()
+    # Subscribe to topic for monitoring harness status
+    harnessMutex = threading.Lock()
+    postHarnessStatus(-1)
+    harnessThread = threading.Thread(target=checkHarnessStatus)
+    harnessThread.start()
 
   elif role == "fieldcomputer":
     rospy.Subscriber("/srcsim/finals/task", Task, fcTaskCallback)
